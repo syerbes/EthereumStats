eda1a5932d82715a308a85cb2bb1728dbb9cd4c2
Fernando Cepeda <onomatopeyalamenor@gmail.com>
2018-03-05 01:30:56 +0100

    Tx full search v1

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -23,20 +23,22 @@ router.get('/', function (req, res, next) {
   });
 });
 
+// Obtener el número del último bloque
 router.get('/lastBlock', function (req, res){
-  
   web3.eth.getBlockNumber().then(console.log);
-  
 });
 
 // Busca las transacciones en el bloque introducido y devuelve el hash de una
 // al azar.
 router.get('/searchBlock', function (req, res){
   var blockNumber = req.query.bnumber;
-  var txlength = 0;
   var lastBlock = req.query.lb;
+  getRandomTx(blockNumber, lastBlock, getTxDetails);
+});
 
-  //console.log('COMENZAMOS');
+// Dado un bloque, devuelve el hash de una transacción del mismo al azar.
+function getRandomTx(blockNumber, lastBlock, callback){
+  var txlength = 0;
   web3.eth.getBlock(blockNumber, false, function (error, result) {
    if (!error && result!=null){
       if (result.transactions.length == 0){
@@ -50,7 +52,7 @@ router.get('/searchBlock', function (req, res){
       var chosenTx = Math.round(chosenTxNumber);
       var txHash = result.transactions[chosenTx];
       console.log('La transacción a trackear es: ' + result.transactions[chosenTx]);
-      getTxDetails(txHash, lastBlock);
+      callback(txHash, lastBlock, getTransactionsByAccount);
    } else {
       if(result == null){
         console.log('El bloque aún no se ha creado.'); 
@@ -59,19 +61,19 @@ router.get('/searchBlock', function (req, res){
       console.log('Ha habido algún problema: ', error); 
    };
   });
-});
+};
 
 // Dado el hash de una transacción, obtiene las wallets origen y destino, junto con
 // el número de bloque. Después, calcula el árbol de transacciones posteriores (desde la wallet
 // destino, así como desde las wallets a las que esta wallet realiza transacciones). La búsqueda se
 // interrumpe al alcanzar el quinto nivel en el árbol de transacciones o al trackear 100000.
-function getTxDetails(tx, lastBlock){
+function getTxDetails(tx, lastBlock, callback){
   web3.eth.getTransaction(tx, function(error, result) {
     if (!error){
       console.log('Dirección origen: ' + result.from);
       console.log('Dirección destino: ' + result.to);
       console.log('El número de bloque es: ' + (result.blockNumber + 1));
-      getTransactionsByAccount(result.to, result.blockNumber + 1, lastBlock, getTransactionsList);
+      callback(result.to, result.blockNumber + 1, lastBlock, getTransactionsList);
     } else {
       console.log('Ha habido algún problema: ', error); 
     };
@@ -90,37 +92,29 @@ function getTransactionsList(myAccount, startBlockNumber, endBlockNumber){
   console.log("Searching for transactions from account \"" + myAccount + "\" within blocks "  + startBlockNumber + " and " + endBlockNumber);
   console.log("La cuenta a buscar es: " + myAccount);
   // Seguimos buscando las demás transacciones
-  var newTx = iterar(myAccount, startBlockNumber, endBlockNumber);
-  //seguir
-
+  iterar(myAccount, startBlockNumber, endBlockNumber, function(newTx){
+      console.log("Las nuevas transacciones a buscar son: " + newTx);
+  });
 };
 
-function iterar(myAccount, startBlockNumber, endBlockNumber){
-  var txToSearch = new Array();
+function iterar(myAccount, startBlockNumber, endBlockNumber, callback){
+  var txList = {};
+  var accToSearch = [];
   for (var i = startBlockNumber; i <= endBlockNumber; i++) {
-      //console.log("startBlockNumber ES: " + startBlockNumber);
       web3.eth.getBlock(i, true, function(error, result){
           //console.log(result.number);
-          //console.log(result.transactions);
           if (result != null && result.transactions != null) {
              result.transactions.forEach( function(e) {
-                if (myAccount == e.from) { //myaccount == "*" || 
-                    txToSearch.push(e);
-                    console.log("  tx hash          : " + e.hash + "\n"
-                     //+ "   nonce           : " + e.nonce + "\n"
-                     //+ "   blockHash       : " + e.blockHash + "\n"
-                     + "   blockNumber     : " + e.blockNumber + "\n"
-                     //+ "   transactionIndex: " + e.transactionIndex + "\n"
-                    + "   from            : " + e.from + "\n" 
-                     //+ "   to              : " + e.to + "\n"
-                      //+ "   value           : " + e.value + "\n"
-                      //+ "   time            : " + block.timestamp + " " + new Date(block.timestamp * 1000).toGMTString() + "\n"
-                    //+ "   gasPrice        : " + e.gasPrice + "\n"
-                    //+ "   gas             : " + e.gas + "\n"
-                   //+ "   input           : " + e.input);
-                    )};
+                //problema
+                if (myAccount == e.from || accToSearch.includes(e.from)) {
+                  // añadimos la nueva cuenta destino en un array, para buscar también por esta
+                    txList[e.hash] = [e.from, e.to, e.blockNumber];
+                    accToSearch.push(e.to);
+                    console.log("AQUI ESTAMOS" + e.to);
+                };
             });
           };
       });
   };
+  callback(txList);
 };
